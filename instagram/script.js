c4015c2c948499a46fd2357a691a700dceb38c92
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-12-14 17:00:15 +0900

    dark mode && 문서 서식 정리
    -dark mode
    : 이미지에도 dark mode 를 넣기위해 svg 형식으로 변경함
    
    -문서 서식 정리

--- instagram/script.js
+++ instagram/script.js
@@ -1,8 +1,25 @@
+/* dark mode */
+
 // Elements
 const toggleThemeBtn = document.querySelector('.head-theme-btn');
+
+document.onload = setIntervalTheme(localStorage.getItem('theme'));
+function setIntervalTheme(themeKey) {
+    if(themeKey === 'dark') {
+        document.documentElement.classList.add('darkTheme');
+    } else {
+        document.documentElement.classList.remove('darkTheme');
+    }
+}
 
 // Toggle theme button
 toggleThemeBtn.addEventListener('click', () => {
     // Toggle root class
     document.documentElement.classList.toggle('darkTheme');
+    // click -> save localStorage
+    if(document.documentElement.classList.contains('darkTheme')) {
+        localStorage.setItem('theme','dark');
+    } else {
+        localStorage.setItem('theme','light');
+    }
 });
